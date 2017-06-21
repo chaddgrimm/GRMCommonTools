3764846c192eb78b6e9f6718a4d3cf7b5097176d
Chad <frederickrichard.lee@gmail.com>
2017-06-21 18:19:02 +0100

    Added height/width constraints getter.

---
+++ GRMCommonTools/UIImage+Geometry.swift
@@ -0,0 +1,27 @@
+//
+//  UIImage+Geometry.swift
+//  Pods
+//
+//  Created by Frederick Lee on 21/06/2017.
+//
+//
+
+import UIKit
+
+extension UIImage {
+    class func circle(diameter: CGFloat, color: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: diameter, height: diameter), false, 0)
+        let ctx = UIGraphicsGetCurrentContext()!
+        ctx.saveGState()
+
+        let rect = CGRect(x: 0, y: 0, width: diameter, height: diameter)
+        ctx.setFillColor(color.cgColor)
+        ctx.fillEllipse(in: rect)
+
+        ctx.restoreGState()
+        let img = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+
+        return img
+    }
+}
